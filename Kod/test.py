ede5eaeb8aca8ec7834a83a5aee07d7bb3a040d4
wpedrak <woj123@gmail.com>
2019-04-25 19:50:03 +0200

    legit function for open door

--- Kod/test.py
+++ Kod/test.py
@@ -1,12 +1,16 @@
 from py532lib.i2c import Pn532_i2c
 from repository import *
+import RPi.GPIO as GPIO
 from time import sleep
 # from py532lib.frame import *
 # from py532lib.constants import *
 
 START_OF_SERIAL = 14
 SERIAL_LENGTH = 8
+RELAY_PIN = 18
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
 
 # def prety(to_prety: str) -> str:
 def prety(to_prety):
@@ -22,24 +26,22 @@ def get_serial(byte_array):
 
 # def open_door() -> None:
 def open_door() -> None:
-    print('door open')
-    sleep(1)
+    GPIO.output(RELAY_PIN, GPIO.LOW)
+    time.sleep(2)
+    GPIO.output(RELAY_PIN, GPIO.HIGH)
 
 # def main() -> None:
 def main():
     pn532 = Pn532_i2c()
     pn532.SAMconfigure()
-
-    while(True):
-        card_data = pn532.read_mifare().get_data()
-        serial_number = get_serial(card_data)
-        if is_authorized(serial_number):
-            open_door()
-        # print(card_data)
-        # print(get_serial(card_data))
-        # print(prety(bytes(card_data).hex()))
-
-        # print([x for x in card_data])
+    try:
+        while(True):
+            card_data = pn532.read_mifare().get_data()
+            serial_number = get_serial(card_data)
+            if is_authorized(serial_number):
+                open_door()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
 
 if __name__ == "__main__":
