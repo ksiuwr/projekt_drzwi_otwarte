8f056920d3969a2ac4e0bffb54d7d7362e5aab8e
Kamil Matuszewski <kamil.k.mat@gmail.com>
2019-05-09 19:13:31 +0200

    Space instead of tab

--- Kod/SQL/1-create_basic_table.sql
+++ Kod/SQL/1-create_basic_table.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS registeredCards;
 CREATE TABLE registeredCards (
     name VARCHAR(100) UNIQUE,
     serial VARCHAR(8) UNIQUE,
-	timestamp DATE DEFAULT (datetime('now','localtime'))
+    timestamp DATE DEFAULT (datetime('now','localtime'))
     );
