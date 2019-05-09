3042e15ea4f935d8a3be53582bb3496a5c4155f6
Arsenicro <kamil.k.mat@gmail.com>
2019-05-09 19:59:47 +0200

    Default value

--- Kod/SQL/1-create_basic_table.sql
+++ Kod/SQL/1-create_basic_table.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS registeredCards;
 CREATE TABLE registeredCards (
     name VARCHAR(100) UNIQUE,
     serial VARCHAR(8) UNIQUE,
-    last_used DATE NOT NULL (datetime('now','localtime')),
+    last_used DATE NOT NULL DEFAULT (datetime('now','localtime')),
     timestamp DATE NOT NULL DEFAULT (datetime('now','localtime'))
     );
