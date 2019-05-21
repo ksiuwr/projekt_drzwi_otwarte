a62eaad9ea744453511805cc6ea9ae6a3d6c1c5d
Arsenicro <kamil.k.mat@gmail.com>
2019-05-21 18:08:08 +0200

    Add error log type

--- Kod/SQL/3-create_log_table.sql
+++ Kod/SQL/3-create_log_table.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS logs;
 CREATE TABLE logs (
-    type VARCHAR(15) NOT NULL CHECK (type IN ('add','open','reject')),
+    type VARCHAR(15) NOT NULL CHECK (type IN ('add','open','reject','error')),
     message VARCHAR(150),
     timestamp DATE DEFAULT (datetime('now','localtime'))
     );
