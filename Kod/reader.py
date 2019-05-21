b08a38ce3285c1832d571afe7f7d650e1f75899d
Wiktor Adamski <wiktor.marek.adamski@gmail.com>
2019-05-21 19:34:00 +0200

    changed connection management in reader

--- Kod/reader.py
+++ Kod/reader.py
@@ -12,11 +12,11 @@ def get_serial(byte_array):
     return hex_data[START_OF_SERIAL:START_OF_SERIAL+SERIAL_LENGTH]
 
 
-connection = Client('/tmp/worker', 'AF_UNIX')
-
 pn532 = Pn532_i2c()
 pn532.SAMconfigure()
 while(True):
     card_data = pn532.read_mifare().get_data()
     serial_number = get_serial(card_data)
+    connection = Client('/tmp/worker', 'AF_UNIX')
     connection.send({'type': 'read', 'value': serial_number})
+    connection.close()
