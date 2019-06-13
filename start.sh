6247cc0e971d23011d4e02b2623f6f7810557294
wpedrak <woj123@gmail.com>
2019-06-13 19:48:31 +0200

    startup stderr

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 function run_module {
     module_name=$1
     rm -f /tmp/${module_name}
-    nohup python3 ${module_name}.py > ${module_name}.out &
+    nohup python3 ${module_name}.py > ${module_name}.out 2> ${module_name}.err &
 }
 
 curr_dir=$(pwd)
-cd $DOOR_HOME
+cd $DOOR_HOME/Kod
 
 run_module worker
 run_module reader
