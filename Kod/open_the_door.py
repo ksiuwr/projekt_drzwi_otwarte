038dffbfcca7e08a485d9add3f48c2c90e10a7a7
Piotr Szymajda <PiotrSzymajda@10g.pl>
2019-08-25 20:40:09 +0200

    Fix typo

--- Kod/open_the_door.py
+++ Kod/open_the_door.py
@@ -4,8 +4,8 @@ import messenger
 
 def main():
     # () -> None
-    opening_time = sys.argv if len(sys.argv) > 1 else "5"
-    messenger.send('/tmp/worker', {'type': 'read', 'value': opening_time})
+    opening_time = sys.argv[1] if len(sys.argv) > 1 else "5"
+    messenger.send('/tmp/worker', {'type': 'open', 'value': opening_time})
 
 
 if __name__ == '__main__':
