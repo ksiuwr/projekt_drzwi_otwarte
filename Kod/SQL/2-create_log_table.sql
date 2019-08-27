55a8890e5955cd3995c1827f53f5bea00bc86180
Arsenicro <kamil.k.mat@gmail.com>
2019-08-27 21:23:14 +0200

    Logging Logs

--- Kod/SQL/2-create_log_table.sql
+++ Kod/SQL/2-create_log_table.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS logs;
 CREATE TABLE logs (
-    type VARCHAR(15) NOT NULL CHECK (type IN ('add','open','reject','error','unknown')),
+    type VARCHAR(15) NOT NULL CHECK (type IN ('add','open','reject','info','error','unknown')),
     message VARCHAR(150),
     timestamp DATE DEFAULT (datetime('now','localtime'))
     );
