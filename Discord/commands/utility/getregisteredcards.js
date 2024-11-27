27a2e9c70c85fbb66bc0e4f246a36df8ab8a923c
Artur Bieniek <ar2rb0k@interia.pl>
2024-11-27 19:27:48 +0100

    Add Discord bot code

---
+++ Discord/commands/utility/getregisteredcards.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, AttachmentBuilder, PermissionFlagsBits } = require('discord.js');
+const { spawnSync } = require("child_process");
+
+module.exports = {
+    cooldown: 6,
+	data: new SlashCommandBuilder()
+	.setName('getregisteredcards')
+	.setDescription('Pobiera listę zarejestrowanych kart.')
+	.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+	    async execute(interaction) {
+		await interaction.deferReply("Sprawdzam...");
+		const child = spawnSync(process.env.DOOR_HOME+"Skrypty/get-registered-cards.sh",  [], { cwd: process.env.DOOR_HOME, stdio: 'pipe', encoding: 'utf-8' });
+		console.log(child.output);
+		//console.log(child.stdio);
+		let atc = new AttachmentBuilder(Buffer.from(child.output[1]), { name:'cards.txt' });
+		interaction.editReply({ files: [atc] });
+		//interaction.editReply(child.output[1]);
+		//await new Promise( (resolve) => {
+		//    await child.on('close', resolve);
+		//}).then(await interaction.reply(child.stdout));
+		//child.stdout.setEncoding('utf8');
+		//child.stdout.on('data', function(data) {
+			//Here is where the output goes
+		//	console.log('stdout: ' + data);
+		//await interaction.reply('Pobieram listę kart...');
+		//});
+             },
+};
